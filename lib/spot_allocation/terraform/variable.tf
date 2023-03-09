b30e8bf66c4c8f4f53b372e1182e9f0b9d119c8e
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2023-03-09 11:06:58 +0530

    Add variables

--- lib/spot_allocation/terraform/variable.tf
+++ lib/spot_allocation/terraform/variable.tf
@@ -1,9 +1,12 @@
-variable "spot_price" {
-  type = string
-  default = "0.1"
-}
-  
 variable "instance_type" {
   type = string
   default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0557a15b87f6559cf"
+}
+
+variable "spot_instances" {
+  type = map
 }
