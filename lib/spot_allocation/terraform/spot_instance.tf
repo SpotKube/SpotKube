dbd7b7c092534c9456054e69e95fdc3e6582db4c
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2023-03-09 11:07:40 +0530

    Attach spots :bug:

--- lib/spot_allocation/terraform/spot_instance.tf
+++ lib/spot_allocation/terraform/spot_instance.tf
@@ -2,3 +2,16 @@ module "env" {
   source = "../../env_setup/terraform"
 }
 
+resource "aws_spot_instance_request" "worker_nodes" {
+  for_each = var.spot_instances
+  ami                    = "${var.ami_id}"
+  spot_price             = "${each.value.spot_price}"
+  instance_type          = "${each.value.instance_type}"
+  spot_type              = "one-time"
+  block_duration_minutes = "120"
+  wait_for_fulfillment   = "true"
+  key_name               = "spot_key"
+
+  security_groups = ["${module.env.aws_subnet.spot_subnet.id}"]
+  # subnet_id = "${aws_subnet.subnet-uno.id}"
+}
