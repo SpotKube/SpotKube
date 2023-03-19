ba8c29aeeab52035d04ca248e2fcb72945497c07
Kavinda Rajapakse <kavindar.18@cse.mrt.ac.lk>
2023-03-19 11:41:06 +0530

    Updatre script

--- lib/env_setup/terraform/scripts/configure_management_node.sh
+++ lib/env_setup/terraform/scripts/configure_management_node.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 set -e
 
+logger "Starting user_data script"
+
 # Install Ansible
 sudo apt update
 sudo apt install -y software-properties-common
@@ -21,11 +23,20 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://
 sudo apt update
 sudo apt install -y terraform
 
-# Generate key pair
-pushd ~./ssh
-ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+# Install zip
+sudo apt install -y unzip
+
+sudo su ubuntu
+cd
 
 # Install AWS CLI
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
+
+# Generate key pair
+pushd ~./ssh
+ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
+popd
+
+logger "End user_data script"
