07a4019f02b91a9933a71fa8f08bc4827e89f644
Kavinda Rajapakse <kavindsr@gmail.com>
2023-03-23 11:56:35 +0530

    Move common code to init

---
+++ lib/management_node/terraform/aws/1-init.tf
@@ -0,0 +1,18 @@
+data "terraform_remote_state" "env_setup" {
+  backend = "s3"
+
+  config = {
+    bucket = "spotkube-terraform-state-bucket"
+    key    = "env_setup-terraform.tfstate"
+    region = "us-west-2"
+   }
+}
+
+data "local_file" "public_key" {
+  filename = "${var.pub_id_file_path}"
+}
+
+resource "aws_key_pair" "key" {
+  key_name   = "spotkube_management_key"
+  public_key = "${data.local_file.public_key.content}"
+}
