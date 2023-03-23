641a1ffb7f84be81534954c3a621304203d47472
Kavinda Rajapakse <kavindsr@gmail.com>
2023-03-23 11:53:38 +0530

    Rename spot tf

--- lib/management_node/terraform/aws/3-spot_instance.tf
+++ lib/management_node/terraform/aws/3-spot_instance.tf
@@ -1,22 +1,3 @@
-data "terraform_remote_state" "env_setup" {
-  backend = "s3"
-
-  config = {
-    bucket = "spotkube-terraform-state-bucket"
-    key    = "env_setup-terraform.tfstate"
-    region = "us-west-2"
-   }
-}
-
-data "local_file" "public_key" {
-  filename = "${var.pub_id_file_path}"
-}
-
-resource "aws_key_pair" "key" {
-  key_name   = "spotkube_management_key"
-  public_key = "${data.local_file.public_key.content}"
-}
-
 resource "aws_spot_instance_request" "worker_nodes" {
   for_each = var.spot_instances
   ami                    = "${var.ami_id}"
