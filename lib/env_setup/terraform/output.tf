7a138a9b0a1b4bd660518f2838d51c737209f5a8
Kavinda Rajapakse <kavindsr@gmail.com>
2023-03-24 14:36:59 +0530

    Output http ingress ids

--- lib/env_setup/terraform/output.tf
+++ lib/env_setup/terraform/output.tf
@@ -2,8 +2,16 @@ output "subnet_id" {
   value = aws_subnet.spot_subnet.id
 }
 
-output "security_group_id" {
+output "security_group_ssh_id" {
   value = aws_security_group.ingress_ssh.id
+}
+
+output "security_group_http_id" {
+  value = aws_security_group.ingress_http.id
+}
+
+output "security_group_https_id" {
+  value = aws_security_group.ingress_https.id
 }
 
 # Save the public ip of the management node
