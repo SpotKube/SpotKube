0d565c2ae3ff36ca670c6da58b43da567bf8e535
Kavinda Rajapakse <kavindsr@gmail.com>
2023-03-24 14:36:07 +0530

    Add new ingress rules

--- lib/env_setup/terraform/1-init.tf
+++ lib/env_setup/terraform/1-init.tf
@@ -80,3 +80,33 @@ resource "aws_security_group" "ingress_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
+resource "aws_security_group" "ingress_http" {
+  name_prefix = "http_ingress_"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "HTTP Ingress"
+  }
+}
+
+resource "aws_security_group" "ingress_https" {
+  name_prefix = "http_ingress_"
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "HTTPS Ingress"
+  }
+}
