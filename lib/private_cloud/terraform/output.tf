e97a53ae1b45f019c86828e89a9d2e043c3cab37
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2023-04-02 18:07:51 +0530

    Naming convention change

--- lib/private_cloud/terraform/output.tf
+++
@@ -1,8 +0,0 @@
-# Output VM IP Address
-output "private_master_ip" {
-  value = openstack_compute_instance_v2.private_master.access_ip_v4
-}
-
-output "private_master_floating_ip" {
- value = openstack_networking_floatingip_v2.fip1.address
-}
--- lib/private_cloud/terraform/variable.tf
+++
@@ -1,5 +0,0 @@
-# Variables
-variable "keypair" {
-  type    = string
-  default = "admin"   # name of keypair created 
-}
