a4021701457ac745f1bb2a65a463d87b5e71c016
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2023-04-02 18:09:59 +0530

    Move api-server and optimization-engine to src

--- optimization-engine/optimizer/optimizerStrategy.py
+++
@@ -1,7 +0,0 @@
-class OptimizerStrategy:
-    def __init__(self, func=None):
-        if func:
-            self.optimize = func
-
-    def optimize(self):
-        print("Concrete function is not initialized")
--- optimization-engine/paths.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-dirs = ['optimizer', 'predictor', 'costModel']
-for i in dirs:
-    package_path = os.path.abspath(i)
-    sys.path.append(package_path)
-
-print(sys.path)
