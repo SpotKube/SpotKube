3ab5b964d817decec6b6f7aea1e8553903dcbfe0
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2023-04-04 18:45:11 +0530

    Output changed to add worker nodes ips

--- src/node_allocator/management_node/terraform/private_cloud/output.tf
+++ src/node_allocator/management_node/terraform/private_cloud/output.tf
@@ -6,3 +6,12 @@ output "private_master_ip" {
 output "private_master_floating_ip" {
  value = openstack_networking_floatingip_v2.floating_ip1.address
 }
+
+output "private_workers" {
+  value = [for instance in openstack_compute_instance_v2.private_workers : {
+    private_ip = instance.access_ip_v4
+  }]
+}
+
+
+ 
