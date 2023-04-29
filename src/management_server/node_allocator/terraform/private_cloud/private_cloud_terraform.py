77ab7da859346a559bdbe768bc6db57f17dd6f6a
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2023-04-29 11:34:51 +0530

    Add status to return

--- src/management_server/node_allocator/terraform/private_cloud/private_cloud_terraform.py
+++ src/management_server/node_allocator/terraform/private_cloud/private_cloud_terraform.py
@@ -16,7 +16,7 @@ async def destroy_private_cloud():
         output = rn_subprocess_cmd(["terraform", "destroy", "-auto-approve", "-var-file=private.tfvars"], cwd=terraform_dir)
         print(output)
         logger.info("Private cloud destroyed")
-        return {"message": "Private cloud destroyed"}
+        return {"message": "Private cloud destroyed", "status": "success"}
     
     except subprocess.CalledProcessError as e:
         # Log the error message and return it
@@ -24,14 +24,14 @@ async def destroy_private_cloud():
         print(error_message)
         error_message = format_terraform_error_message(error_message)
         logger.error(error_message)
-        return {"error_message": error_message}
+        return {"error_message": error_message, "status": "failed"}
     
     except  Exception as e:
         # Print and Log the error message and return it
         print(e)
         error_message = format_terraform_error_message(str(e))
         logger.error(error_message)
-        return {"error_message": str(e)}
+        return {"error_message": str(e), "status": "failed"}
     
 
 async def destroy_and_provision_private_cloud():
@@ -58,13 +58,13 @@ async def destroy_and_provision_private_cloud():
             data = json.load(f)
             management_node_floating_ip = data["private_management_floating_ip"]["value"]
             
-        return {"management_node_floating_ip": management_node_floating_ip}
+        return {"management_node_floating_ip": management_node_floating_ip, "status": "success"}
     except subprocess.CalledProcessError as e:
         # Log the error message and return it
         error_message = e.output.decode("utf-8")
         with open(f"{terraform_dir}/terraform_error_output.txt", "w") as f:
             f.write(error_message)
-        return {"error_message": error_message}
+        return {"error_message": error_message, "status": "failed"}
     
     
 # Initialize Terraform and apply changes
@@ -73,25 +73,25 @@ async def provision_private_cloud():
         # Initialize Terraform
         subprocess.run(["terraform", "init"], cwd=terraform_dir)
         await apply_terraform()
-        return {"message": "Private cloud provisioned"}
+        return {"message": "Private cloud provisioned", "status": "success"}
     except subprocess.CalledProcessError as e:
         # Log the error message and return it
         error_message = e.output.decode("utf-8")
         with open(f"{terraform_dir}/terraform_error_output.txt", "w") as f:
             f.write(error_message)
-        return {"error_message": error_message}
+        return {"error_message": error_message, "status": "failed"}
 
 # Apply changes
 async def apply_private_cloud():
     try:
         await apply_terraform()
-        return {"message": "Private cloud changes applied"}
+        return {"message": "Private cloud changes applied", "status": "success"}
     except subprocess.CalledProcessError as e:
         # Log the error message and return it
         error_message = e.output.decode("utf-8")
         with open(f"{terraform_dir}/terraform_error_output.txt", "w") as f:
             f.write(error_message)
-        return {"error_message": error_message}
+        return {"error_message": error_message, "status": "failed"}
 
 # Private function to apply changes
 async def apply_terraform():
