651e9bedc672f7e8a7426255b1181a57a5d1ac44
Kavinda Rajapakse <kavindsr@gmail.com>
2023-05-03 21:28:29 +0530

    Add function to get pods cpu usage

--- src/elastic_scalar/pkg/kube/get_cluster_metrics.go
+++ src/elastic_scalar/pkg/kube/get_cluster_metrics.go
@@ -7,12 +7,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-type nodeCpuUsage struct {
-	nodeName string
-	cpuUsage float64
-	totalCps float64
-}
-
 func GetNodeCpuUsage() []nodeCpuUsage {
 	nodes := GetNodes()
 	nodesCpuUsage := make([]nodeCpuUsage, len(nodes.Items))
@@ -20,18 +14,41 @@ func GetNodeCpuUsage() []nodeCpuUsage {
 	for i, node := range nodes.Items {
 		nodeMetrics, err := metricsClientset.MetricsV1beta1().NodeMetricses().Get(context.Background(), node.ObjectMeta.Name, metav1.GetOptions{})
 		if err != nil {
-			panic(err.Error())
+			// panic(err.Error())
+			continue
 		}
 		cpuUsage := nodeMetrics.Usage.Cpu()
 		fmt.Printf("Node %s CPU usage: %f\n", node.ObjectMeta.Name, cpuUsage.AsDec())
 		totalCpu := node.Status.Capacity.Cpu()
-		cpuUsagePercent := float64(cpuUsage.MilliValue()) / float64(totalCpu.MilliValue()) * 100
-		fmt.Printf("Node %s CPU percentage: %f\n", node.ObjectMeta.Name, cpuUsagePercent)
+		fmt.Printf("Node %s total CPU: %f\n", node.ObjectMeta.Name, float64(totalCpu.MilliValue()))
 		nodesCpuUsage[i] = nodeCpuUsage{
 			nodeName: node.ObjectMeta.Name,
 			cpuUsage: float64(cpuUsage.MilliValue()),
-			totalCps: float64(totalCpu.MilliValue()),
+			totalCpu: float64(totalCpu.MilliValue()),
 		}
 	}
 	return nodesCpuUsage
+}
+
+func GetPodCpuUsage() []podCpuUsage {
+	pods := GetPods()
+	podsCpuUsage := make([]podCpuUsage, len(pods.Items))
+	for i, pod := range pods.Items {
+		podMetrics, err := metricsClientset.MetricsV1beta1().PodMetricses(namespace).Get(context.Background(), pod.ObjectMeta.Name, metav1.GetOptions{})
+		if err != nil {
+			// panic(err.Error())
+			continue
+		}
+		cpuUsage := podMetrics.Containers[0].Usage.Cpu()
+		fmt.Printf("Pod %s CPU usage: %f\n", pod.ObjectMeta.Name, cpuUsage.AsDec())
+		// Get the total CPU allocated to the pod
+		totalCpu := pod.Spec.Containers[0].Resources.Requests.Cpu()
+		fmt.Printf("Pod %s total CPU allocation: %f\n", pod.ObjectMeta.Name, float64(totalCpu.MilliValue()))
+		podsCpuUsage[i] = podCpuUsage{
+			podName:  pod.ObjectMeta.Name,
+			cpuUsage: float64(cpuUsage.MilliValue()),
+			totalCpu: float64(totalCpu.MilliValue()),
+		}
+	}
+	return podsCpuUsage
 }
