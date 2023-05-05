020f02598e3d77c5049128480a3d09c99c860b0e
PasinduYeshan <pasindua.18@cse.mrt.ac.lk>
2023-05-05 11:01:04 +0530

    Add uninstall_helm_charts

--- src/management_server/helm_service/router.py
+++ src/management_server/helm_service/router.py
@@ -13,7 +13,10 @@ async def root():
     return {"message": "This is the helm service private cloud router"}
 
 @helm_router.get("/deploy")
-async def route_provision_private_cloud():
+async def deploy_helm_charts():
     return await deploy_helm_charts()
 
+@helm_router.get("/uninstall")
+async def uninstall_helm_charts():
+    return await uninstall_helm_charts()
     
--- src/management_server/helm_service/service.py
+++ src/management_server/helm_service/service.py
@@ -38,3 +38,32 @@ async def deploy_helm_charts():
         error_message = format_terraform_error_message(str(error))
         logger.error(error_message)
         return {"error_message": error_message, "status": "failed"}
+    
+async def uninstall_helm_charts():
+    try:
+        # Load the config.yml file
+        with open('~/.config/spotkube/config.yml') as f:
+            config = yaml.safe_load(f)
+
+        # Loop through each service and install the corresponding Helm chart
+        for service in config['services']:
+            helm_chart_path = service['helmChartPath']
+            pod_count = service['minRPS']['pods']
+            service_name = service['name']
+            
+            run_subprocess_popen_cmd(["helm", "uninstall", "{helm_chart_path}"], cwd=current_dir)        
+            # os.system(f"helm upgrade --install --set replicaCount={pod_count} {service_name} {helm_chart_path}")
+    
+    except subprocess.CalledProcessError as e:
+        # Log the error message and return it
+        error_message = e.output.decode("utf-8")
+        print(error_message)
+        error_message = format_terraform_error_message(str(error_message))
+        logger.error(error_message)
+        return {"error_message": error_message, "status": "failed"}
+    
+    except  Exception as error:
+        print(error)
+        error_message = format_terraform_error_message(str(error))
+        logger.error(error_message)
+        return {"error_message": error_message, "status": "failed"}
