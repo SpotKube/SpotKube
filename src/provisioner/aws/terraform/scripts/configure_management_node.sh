e0aa8b8da9f227fbf8641d4f8245666b3e062baa
Kavinda Rajapakse <kavindsr@gmail.com>
2023-05-21 15:04:15 +0530

    Install helm

--- src/provisioner/aws/terraform/scripts/configure_management_node.sh
+++ src/provisioner/aws/terraform/scripts/configure_management_node.sh
@@ -5,6 +5,8 @@ source "../../../common/scripts/common_pkg_installer.sh"
 
 # Create anisble config file
 sudo apt-get update
+
+rm -f ~/.ansible.cfg
 touch ~/.ansible.cfg
 echo "[defaults]" >> ~/.ansible.cfg
 echo "host_key_checking = False" >> ~/.ansible.cfg
@@ -20,3 +22,15 @@ ssh-keygen -q -t rsa -N '' -f ~/.ssh/id_rsa <<<y >/dev/null 2>&1
 
 # Create directory if not exists
 mkdir -p ~/.aws
+
+# Install helm
+curl https://baltocdn.com/helm/signing.asc | gpg --dearmor | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
+sudo apt-get install apt-transport-https --yes
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm --yes
+
+# Cleanup
+rm -rf awscliv2.zip aws kubectl
+
+print_info "Instalation completed successfully"
