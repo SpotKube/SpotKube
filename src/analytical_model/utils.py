7de140f9e23db69f80f693b34fa695ac0960405b
Kavinda Rajapakse <kavindsr@gmail.com>
2023-06-07 16:25:51 +0530

    Add function to write to the output

--- src/analytical_model/utils.py
+++ src/analytical_model/utils.py
@@ -3,11 +3,12 @@ from collections import defaultdict
 import json
 import yaml
 import os
+from ruamel.yaml import YAML
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(dir_path, '../../.config/config.yml')
 
 def getMinRps():
-    file_path = os.path.join(dir_path, '../../../.config/config.yml')
     services = []
     with open(file_path, "r") as stream:
         try:
@@ -20,4 +21,20 @@ def getMinRps():
     return services
 
 
-
+def updatePodCount(service, count):
+    yaml_ruamel=YAML(typ='safe')
+    yaml_ruamel.default_flow_style = False
+    yaml_ruamel.preserve_quotes = True
+    yaml_ruamel.sort_keys = False
+    try:
+        with open(file_path, 'r') as f:
+            data = yaml_ruamel.load(f)
+        
+        for serviceDic in data['services']:
+            if(service == serviceDic['name']):
+                serviceDic['minRPS']['pods'] = count
+                break
+        with open(file_path, 'w') as f:
+            yaml_ruamel.dump(data,f)
+    except yaml_ruamel.YAMLError as exc:
+        print(exc)
