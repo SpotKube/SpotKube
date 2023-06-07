2d2201c55b3a5a346d594bd8607243c7de109246
Kavinda Rajapakse <kavindsr@gmail.com>
2023-06-07 19:58:49 +0530

    handle more inputs

--- src/analytical_model/utils.py
+++ src/analytical_model/utils.py
@@ -6,22 +6,24 @@ import os
 from ruamel.yaml import YAML
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(dir_path, '../../.config/config.yml')
+
 
 def getMinRps():
+    file_path = os.path.join(dir_path, '../../.config/user_config.yml')
     services = []
     with open(file_path, "r") as stream:
         try:
             data = yaml.safe_load(stream)
             for service in data['services']:
-                services.append({'name':service['name'], 'minRps': service['minRPS']['rps']})
+                services.append({'name':service['name'], 'minRPS': service['minRPS'], 'private': service['private'], 'helmChartPath': service['helmChartPath']})
         except yaml.YAMLError as exc:
             print(exc)    
     
     return services
 
 
-def updatePodCount(service, count):
+def updatePodCount(service, count, rps, private, helmChartPath):
+    file_path = os.path.join(dir_path, '../../.config/config.yml')
     yaml_ruamel=YAML(typ='safe')
     yaml_ruamel.default_flow_style = False
     yaml_ruamel.preserve_quotes = True
@@ -33,10 +35,12 @@ def updatePodCount(service, count):
         for serviceDic in data['services']:
             if(service == serviceDic['name']):
                 serviceDic['minRPS']['pods'] = count
+                serviceDic['minRPS']['rps'] = rps
+                serviceDic['private'] = private
+                serviceDic['helmChartPath'] = helmChartPath
                 break
         else:
-            print("Service not found")
-            return
+            data['services'].append({'name': service, 'minRPS': {'pods': count, 'rps': rps}, 'private': private})
         with open(file_path, 'w') as f:
             yaml_ruamel.dump(data,f)
     except yaml_ruamel.YAMLError as exc:
