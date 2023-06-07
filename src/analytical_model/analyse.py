a5d75ec7988f2778d25b0d3ac4bcc460c7c7ae44
Kavinda Rajapakse <kavindsr@gmail.com>
2023-06-07 16:27:17 +0530

    Add main function to run analytical model

---
+++ src/analytical_model/analyse.py
@@ -0,0 +1,17 @@
+import os
+import sys
+dirs = ['analytical_model']
+for i in dirs:
+    package_path = os.path.abspath(i)
+    sys.path.append(package_path)
+from pod_calculator import calculator
+import utils
+
+from metric_analyser import cpuAnalyser, memoryAnalyser, preprocess
+# Iterate thorugh directories in outputs directory
+for service in os.listdir('../load_testing/outputs'):
+    serviceTuple = calculator.calculate(service)
+    print(serviceTuple)
+    utils.updatePodCount(serviceTuple[0], serviceTuple[1])
+
+ 
