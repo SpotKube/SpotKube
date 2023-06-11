41ef7b7ca7d8e8d533a6db3e28c388daac2b9a74
Kavinda Rajapakse <kavindsr@gmail.com>
2023-06-12 00:02:16 +0530

    Change nginx config

--- src/provisioner/aws/terraform/scripts/configure_reverse_proxy.sh
+++ src/provisioner/aws/terraform/scripts/configure_reverse_proxy.sh
@@ -3,34 +3,22 @@
 management_node="$1"
 
 # Change directory to /etc/nginx
-cd /etc/nginx
+cd /etc/nginx/sites-available
 
-# Create the 'ssl' directory
-sudo mkdir ssl
-
-# Generate SSL certificate
-sudo openssl req -batch -x509 -nodes -days 365 \
--newkey rsa:2048 \
--keyout /etc/nginx/ssl/server.key \
--out /etc/nginx/ssl/server.crt
-
-# Change directory to /etc/nginx/sites-enabled/
-cd /etc/nginx/sites-enabled/
-
-# Create or edit the 'fastapi_nginx' file
-cat <<EOF | sudo tee fastapi_nginx
+# Create or edit the 'mgt_server' file
+cat <<EOF | sudo tee spotkube
+# Management server API
 server {
     listen 80;
-    listen 443 ssl;
-    ssl on;
-    ssl_certificate /etc/nginx/ssl/server.crt;
-    ssl_certificate_key /etc/nginx/ssl/server.key;
-    server_name $management_node;
+    server_name ${MGT_SERVER};
     location / {
-        proxy_pass http://127.0.0.1:8000;
+        proxy_pass http://localhost:8000;
     }
 }
 EOF
 
+# Create symlink
+sudo ln -s /etc/nginx/sites-available/spotkube /etc/nginx/sites-enabled/
+
 # Restart the nginx service
-sudo service nginx restart
+sudo systemctl restart nginx.service
