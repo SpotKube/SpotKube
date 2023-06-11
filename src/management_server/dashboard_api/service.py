7201a1691a7d59097842a98e8aebbdb298e4aba4
Kavinda Rajapakse <kavindsr@gmail.com>
2023-06-11 18:34:38 +0530

    Change api output

--- src/management_server/dashboard_api/service.py
+++ src/management_server/dashboard_api/service.py
@@ -4,24 +4,29 @@ region = 'us-east-1'
 
 def get_spot_instances():
     running_instances = get_running_instances(region)
-    spot_instances = []
-    on_demand_pricing = {}
-    spot_pricing = {}
+    spot_instances = {}
+    spot_instances_arr = []
+
     for instance in running_instances:
         for tag in instance['tags']:
             if tag['Key'] != 'Name':
                 continue
             if tag['Value'] not in ['spotkube_master_node', 'spotkube_managment_node']: # 'spotkube_worker_node',
-                if instance['instanceType'] not in on_demand_pricing:
-                    on_demand_pricing[instance['instanceType']] = get_on_demand_pricing(instance['instanceType'])
-                if instance['instanceType'] not in spot_pricing:
-                    spot_pricing[instance['instanceType']] = get_spot_pricing(instance['instanceType'], region)
-                ec2Details = {
-                    'instanceId': instance['instanceId'],
-                    'instanceType': instance['instanceType'],
-                    'onDemandPricing': on_demand_pricing[instance['instanceType']],
-                    'spotPricing': spot_pricing[instance['instanceType']],
-                } 
-                spot_instances.append(ec2Details)
+                if instance['instanceType'] not in spot_instances:
+
+                    spot_instances[instance['instanceType']] = {
+                        'instanceType': instance['instanceType'],
+                        'count': 1,
+                        'onDemandPricing': get_on_demand_pricing(instance['instanceType']),
+                        'spotPricing': get_spot_pricing(instance['instanceType'], region),
+                    }
+                else:
+                    spot_instances[instance['instanceType']]['count'] += 1
+
             break
-    return spot_instances
+
+    # Create spot_instances_arr array from spot_instances dict
+    for key, value in spot_instances.items():
+        spot_instances_arr.append(value)
+
+    return spot_instances_arr
