066a74dc42a0a1ee0f10ee35192074671299948c
Kavinda Rajapakse <kavindsr@gmail.com>
2023-06-16 02:47:53 +0530

    Add label to ec2

---
+++ src/management_server/monitoring/scripts/configure.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+# Import common functions
+source ../../../scripts/common.sh
+
+worker_ip=$(cat ../../node_allocator/terraform/aws/public_env_terraform_output.json| jq -r '.worker_node_ip.value')
+
+# Exit if worker IP is empty
+if [ -z "$worker_ip" ]; then
+    echo "Worker IP is empty"
+    exit 1
+fi
+
+# Extract node name from worker IP
+node_name=$(kubectl get nodes -o json | jq -r '.items[] | select(."metadata"."annotations"."flannel.alpha.coreos.com/public-ip"=="'$worker_ip'") | .metadata.name')
+
+# Exit if node name is empty
+if [ -z "$node_name" ]; then
+    echo "Node name is empty"
+    exit 1
+fi
+
+# Attach label to node
+kubectl label node $node_name monitor=True
