07aee489bb9424a1f827e82c3d397c658597c0b5
Kavinda Rajapakse <kavindsr@gmail.com>
2023-06-16 08:51:18 +0530

    Comment scale down

--- src/elastic_scalar/pkg/optimizer/optmizer.go
+++ src/elastic_scalar/pkg/optimizer/optmizer.go
@@ -114,7 +114,7 @@ func Run() {
 
 	totalCpuUsage := calculateTotalCpuUsage(namespaces)
 	totalCpuCapacity := calculateTotalCpuCapacity()
-	if totalCpuUsage.TotalCpu > 0.8*totalCpuCapacity {
+	if totalCpuUsage.TotalCpu > 0.5*totalCpuCapacity {
 		log.Warn("Total CPU usage is greater than total CPU capacity")
 		// Invoke optimization engine to scale up
 		log.Info("Invoking optimization engine")
@@ -122,8 +122,8 @@ func Run() {
 	} else if totalCpuUsage.TotalCpu < 0.5*totalCpuCapacity {
 		log.Warn("Total CPU usage is less than 50% of total CPU capacity")
 		// Invoke optimization engine to scale down
-		log.Info("Invoking optimization engine")
-		api.InvokeOptimizationEngine(totalCpuUsage.Services, totalCpuUsage.CpuUsageOfpodsInOtherNs, totalCpuUsage.CpuUsageOfDSInOtherNs)
+		// log.Info("Invoking optimization engine")
+		// api.InvokeOptimizationEngine(totalCpuUsage.Services, totalCpuUsage.CpuUsageOfpodsInOtherNs, totalCpuUsage.CpuUsageOfDSInOtherNs)
 	} else {
 		log.Info("Total CPU usage is less than total CPU capacity")
 	}
