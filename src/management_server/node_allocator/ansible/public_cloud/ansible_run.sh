bff616335facee51949167c48c70ef19683f5b60
Kavinda Rajapakse <kavindsr@gmail.com>
2023-06-21 00:21:55 +0530

    Remove notready nodes

--- src/management_server/node_allocator/ansible/public_cloud/ansible_run.sh
+++ src/management_server/node_allocator/ansible/public_cloud/ansible_run.sh
@@ -5,3 +5,7 @@ ansible-playbook -i hosts kube_dependencies.yml
 ansible-playbook -i hosts control_plane.yml
 ansible-playbook -i hosts workers.yml
 ansible-playbook -i hosts setup_kubectl.yml
+
+# Remove NotReady nodes
+kubectl delete node $(kubectl get nodes | grep NotReady | awk '{print $1;}')
+
